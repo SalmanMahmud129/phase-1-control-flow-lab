decdddc9682c769506ab55e3513ada3982ab8cc2
Salman Mahmud <salman_mahmud@hotmail.com>
2022-09-19 17:28:51 -0400

    commit tree 6801211e8a633813ca2c208226b752129f7f0949

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(num){
+  if(num < 400){
+    return 'This one is on me!'
+  }
+
+  else if(num > 400 && num < 2000){
+    return 'That will be twenty bucks.'
+  }
+
+  else if(num > 2000 && num < 2500){
+    return 'I will gladly take your thirty bucks.'
+  }
+
+  else if(num > 2500){
+    return 'No can do.'
+  }
   // Write your code here!
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city == 'NYC'? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(generosity){
   // Write your code here!
+  switch(generosity){
+    case "generous":
+      return "Thank you so much."
+      break
+    case "not as generous":
+      return "Thank you."
+      break
+    case "thanks for everything":
+      return "Bye."
+      break
+  }
 }
